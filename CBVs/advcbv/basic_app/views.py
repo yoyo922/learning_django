7038492f03f83ba5f32c9eb66dbb068c91fc8ada
Peter <zhoupeter2012@gmail.com>
2018-09-27 23:53:11 -0400

    CBV

---
+++ CBVs/advcbv/basic_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import View,TemplateView,ListView,DetailView
+from . import models
+
+class SchoolListView(ListView):
+    model = models.School
+
+class SchoolDetailView(DetailView):
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
