c09d45715dee81fa2eb9147f758ebb79a3dd5a67
Peter <zhoupeter2012@gmail.com>
2018-10-01 01:52:39 -0400

    finish CBV

--- CBVs/advcbv/basic_app/views.py
+++ CBVs/advcbv/basic_app/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from django.views.generic import View,TemplateView,ListView,DetailView
 from . import models
 
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
 class SchoolListView(ListView):
+    context_object_name = 'schools'
     model = models.School
 
 class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
     model = models.School
     template_name = 'basic_app/school_detail.html'
