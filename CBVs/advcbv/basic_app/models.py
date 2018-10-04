5ddd35be75ca4c61c20639bdb76b9906c49b12a5
Peter <zhoupeter2012@gmail.com>
2018-10-04 17:33:14 -0400

    crud views

--- CBVs/advcbv/basic_app/models.py
+++ CBVs/advcbv/basic_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.urlresolvers import reverse
 # Create your models here.
 class School(models.Model):
     name = models.CharField(max_length=256)
@@ -8,6 +8,10 @@ class School(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("basic_app:detail",kwargs={'pk': self.pk})
+
 
 class Student(models.Model):
     name = models.CharField(max_length=256)
--- CBVs/advcbv/basic_app/views.py
+++ CBVs/advcbv/basic_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView,ListView,DetailView
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import View,TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView
 from . import models
 
 class IndexView(TemplateView):
@@ -7,9 +8,21 @@ class IndexView(TemplateView):
 
 class SchoolListView(ListView):
     context_object_name = 'schools'
-    model = models.School
+    model =  models.School
 
 class SchoolDetailView(DetailView):
     context_object_name = 'school_detail'
     model = models.School
     template_name = 'basic_app/school_detail.html'
+
+class SchoolCreateView(CreateView):
+    fields = ('name','principal','location')
+    model = models.School
+
+class SchoolUpdateView(UpdateView):
+    fields = ('principal','name')
+    model = models.School
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
